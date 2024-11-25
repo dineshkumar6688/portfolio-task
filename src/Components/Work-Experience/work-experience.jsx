a5fca6ad22a56ba88cf007cbcd0aca38eddbced3
dineshkumar6688 <dkdinesh6688@gmail.com>
2024-11-25 22:01:41 +0530

    fix: minor changes

--- src/Components/Work-Experience/work-experience.jsx
+++ src/Components/Work-Experience/work-experience.jsx
@@ -17,130 +17,128 @@ const WorkExperiences = () => {
   return (
     <div>
       <div id="experiences">
-        <div class="fh5co-bg-color ">
-          <div class="container">
-            <div class="text-center mt-5">
-              <h2>My Resume</h2>
-            </div>
-            <motion.div
-              variants={horizantalLineVariants}
-              initial="hidden"
-              whileInView="visible"
-              class="horizontal-line"
-            ></motion.div>
-            <div class="row">
-              <div class="col-md-12 col-md-offset-0">
-                <ul class="timeline">
-                  <li class="timeline-heading text-center animate-box">
-                    <div>
-                      <h3>Work Experience</h3>
+        <div class="container">
+          <div class="text-center mt-5">
+            <h2>My Resume</h2>
+          </div>
+          <motion.div
+            variants={horizantalLineVariants}
+            initial="hidden"
+            whileInView="visible"
+            class="horizontal-line"
+          ></motion.div>
+          <div class="row">
+            <div class="col-md-12 col-md-offset-0">
+              <ul class="timeline">
+                <li class="timeline-heading text-center animate-box">
+                  <div>
+                    <h3>Work Experience</h3>
+                  </div>
+                </li>
+                <li class="timeline-inverted animate-box">
+                  <div class="timeline-badge">
+                    <i class="fa-solid fa-suitcase"></i>
+                  </div>
+                  <div class="timeline-panel">
+                    <div class="timeline-heading">
+                      <h3 class="timeline-title">Chief Technology Officer</h3>
+                      <span class="company">
+                        Persist Ventures - Apr 2024 - Present
+                      </span>
                     </div>
-                  </li>
-                  <li class="timeline-inverted animate-box">
-                    <div class="timeline-badge">
-                      <i class="fa-solid fa-suitcase"></i>
+                    <div class="timeline-body">
+                      <p>Full Time</p>
                     </div>
-                    <div class="timeline-panel">
-                      <div class="timeline-heading">
-                        <h3 class="timeline-title">Chief Technology Officer</h3>
-                        <span class="company">
-                          Persist Ventures - Apr 2024 - Present
-                        </span>
-                      </div>
-                      <div class="timeline-body">
-                        <p>Full Time</p>
-                      </div>
+                  </div>
+                </li>
+                <li class="animate-box timeline-unverted">
+                  <div class="timeline-badge">
+                    <i class="fa-solid fa-suitcase"></i>
+                  </div>
+                  <div class="timeline-panel">
+                    <div class="timeline-heading">
+                      <h3 class="timeline-title">CTO @FacesearchAI</h3>
+                      <span class="company">
+                        Persist Ventures - Nov 2023 - Present
+                      </span>
                     </div>
-                  </li>
-                  <li class="animate-box timeline-unverted">
-                    <div class="timeline-badge">
-                      <i class="fa-solid fa-suitcase"></i>
+                    <div class="timeline-body">
+                      <p>FullTime</p>
                     </div>
-                    <div class="timeline-panel">
-                      <div class="timeline-heading">
-                        <h3 class="timeline-title">CTO @FacesearchAI</h3>
-                        <span class="company">
-                          Persist Ventures - Nov 2023 - Present
-                        </span>
-                      </div>
-                      <div class="timeline-body">
-                        <p>FullTime</p>
-                      </div>
+                  </div>
+                </li>
+                <li class="timeline-inverted animate-box">
+                  <div class="timeline-badge">
+                    <i class="fa-solid fa-suitcase"></i>
+                  </div>
+                  <div class="timeline-panel">
+                    <div class="timeline-heading">
+                      <h3 class="timeline-title">Full Stack Engineer</h3>
+                      <span class="company">
+                        Persist Ventures - Oct 2023 - Present
+                      </span>
                     </div>
-                  </li>
-                  <li class="timeline-inverted animate-box">
-                    <div class="timeline-badge">
-                      <i class="fa-solid fa-suitcase"></i>
+                    <div class="timeline-body">
+                      <p>Full Time</p>
                     </div>
-                    <div class="timeline-panel">
-                      <div class="timeline-heading">
-                        <h3 class="timeline-title">Full Stack Engineer</h3>
-                        <span class="company">
-                          Persist Ventures - Oct 2023 - Present
-                        </span>
-                      </div>
-                      <div class="timeline-body">
-                        <p>Full Time</p>
-                      </div>
+                  </div>
+                </li>
+                <li class="animate-box timeline-unverted">
+                  <div class="timeline-badge">
+                    <i class="fa-solid fa-suitcase"></i>
+                  </div>
+                  <div class="timeline-panel">
+                    <div class="timeline-heading">
+                      <h3 class="timeline-title">AI Software Developer</h3>
+                      <span class="company">
+                        HeyDaw Technologies - Aug 2023 - Oct 2023
+                      </span>
                     </div>
-                  </li>
-                  <li class="animate-box timeline-unverted">
-                    <div class="timeline-badge">
-                      <i class="fa-solid fa-suitcase"></i>
+                    <div class="timeline-body">
+                      <p>Internship</p>
                     </div>
-                    <div class="timeline-panel">
-                      <div class="timeline-heading">
-                        <h3 class="timeline-title">AI Software Developer</h3>
-                        <span class="company">
-                          HeyDaw Technologies - Aug 2023 - Oct 2023
-                        </span>
-                      </div>
-                      <div class="timeline-body">
-                        <p>Internship</p>
-                      </div>
+                  </div>
+                </li>
+                <li class="timeline-inverted animate-box">
+                  <div class="timeline-badge">
+                    <i class="fa-solid fa-suitcase"></i>
+                  </div>
+                  <div class="timeline-panel">
+                    <div class="timeline-heading">
+                      <h3 class="timeline-title">Software Engineer</h3>
+                      <span class="company">
+                        Telaverge Communications - May 2022 - Apr 2023
+                      </span>
                     </div>
-                  </li>
-                  <li class="timeline-inverted animate-box">
-                    <div class="timeline-badge">
-                      <i class="fa-solid fa-suitcase"></i>
+                    <div class="timeline-body">
+                      <p>Internship</p>
                     </div>
-                    <div class="timeline-panel">
-                      <div class="timeline-heading">
-                        <h3 class="timeline-title">Software Engineer</h3>
-                        <span class="company">
-                          Telaverge Communications - May 2022 - Apr 2023
-                        </span>
-                      </div>
-                      <div class="timeline-body">
-                        <p>Internship</p>
-                      </div>
-                    </div>
-                  </li>
+                  </div>
+                </li>
 
-                  <br />
-                  <li class="timeline-heading text-center animate-box">
-                    <div>
-                      <h3>Education</h3>
+                <br />
+                <li class="timeline-heading text-center animate-box">
+                  <div>
+                    <h3>Education</h3>
+                  </div>
+                </li>
+                <li class="animate-box timeline-unverted">
+                  <div class="timeline-badge">
+                    <i class="fa-solid fa-graduation-cap"></i>
+                  </div>
+                  <div class="timeline-panel">
+                    <div class="timeline-heading">
+                      <h3 class="timeline-title">Graduation</h3>
+                      <span class="company">
+                        Dr. A.P.J Abdul kalam Technical University
+                      </span>
                     </div>
-                  </li>
-                  <li class="animate-box timeline-unverted">
-                    <div class="timeline-badge">
-                      <i class="fa-solid fa-graduation-cap"></i>
+                    <div class="timeline-body">
+                      <p>Bachelor of Technology - B.Tech, Computer Science</p>
                     </div>
-                    <div class="timeline-panel">
-                      <div class="timeline-heading">
-                        <h3 class="timeline-title">Graduation</h3>
-                        <span class="company">
-                          Dr. A.P.J Abdul kalam Technical University
-                        </span>
-                      </div>
-                      <div class="timeline-body">
-                        <p>Bachelor of Technology - B.Tech, Computer Science</p>
-                      </div>
-                    </div>
-                  </li>
-                </ul>
-              </div>
+                  </div>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
