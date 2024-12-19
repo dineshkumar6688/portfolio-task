71f6c47cf2649ef2a5f174426622c30dbccea9a9
dineshkumar6688 <dkdinesh6688@gmail.com>
2024-12-19 22:20:43 +0530

    fix: minor

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   // assetsInclude: ["**/*.html"],
-  base:'/portfolio-task/'
+  base:'/portfolio-task'
 });
